8ddd7094460d03c47a38787ca0a98a3e7d8e9bac
Kellen Busby <Kellen@Kellens-MacBook-Air.local>
2017-01-13 10:44:24 -0500

    cleaned up a bit and added sounds!

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
 
 // Initialize express
 const app = express();
@@ -12,7 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.set('port', process.env.PORT || 3000);
 
-
+// Make a bunch of folders public for including scripts via html
 app.use('/bower_components', express.static('bower_components'));
 app.use('/lib', express.static('lib'));
 app.use(express.static('public'));
@@ -22,3 +24,47 @@ let server = app.listen(app.get('port'), () => {
 	let port = server.address().port;
 	console.log('listening on port %s', port);
 });
+
+// set up nomad
+const Nomad = require('nomad-stream');
+const nomad = new Nomad();
+
+nomad.subscribe('QmezyLTWQJKjnK2qXwQDgmTwP5LBjayrVkqzUwmV5Sfr1o', obj => {
+	console.log('got nomad message: ' + JSON.stringify(obj));
+
+	if(obj.message) {
+		obj.message = JSON.parse(obj.message); //parse json
+		if(obj.message.event == 'device_on_off') {
+			if(obj.message.data) { // device is on, decrease white fang
+				console.log('emitting decrease-white-fang');
+				eventEmitter.emit('decrease-white-fang');
+			} else {
+				console.log('emitting complete-task');
+				eventEmitter.emit('complete-task', 'TV'); //default to the device being the TV for the purpose of demo...
+			}
+		}
+	}
+});
+
+// setTimeout(function() {
+// 	console.log('emitting fake event!');
+// 	eventEmitter.emit('nomad-publish-event', {
+// 		event: 'decrease-white-fang'
+// 	})
+// }, 5000);
+
+// set up socket.io
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+	console.log('connected to client');
+})
+
+eventEmitter.on('nomad-publish-event', function(obj) {
+	console.log('emitting socket.io event. Object looks like this: ' + JSON.stringify(obj));
+	if(obj.data){
+		io.emit(obj.event, obj.data);
+	} else {
+		io.emit(obj.event);
+	}
+})
